4968cfdd1260ae7bf672d9c86f2e13b653eb4c3c
asazonov28 <80924347+asazonov28@users.noreply.github.com>
2021-04-01 13:03:17 +0300

    update main.py

--- Student/TEST/TEST1.py
+++ Student/TEST/TEST1.py
@@ -1,25 +1,46 @@
+# 1
 # a = input('Enter your name: ')
 # b = input('Enter your surname: ')
 # c = input('Enter your age: ')
 #
 # print(input('Hallo ' + a + ' ' + b + '.' 'You age are ' + c + '.'))
 
+# 2
 # print('triangle_area')
 # a, b = input('Please enter cathetus A, B Use spase separator: ').split()
 # hypotenuse_length = (((int(a) ** 2) + (int(b) ** 2)) ** 0.5)
 # print(hypotenuse_length)
 
-# print('equilateral_triangle')
+# 3
+# print('right_triangle')
 # a, b, c = input('Please enter side A, B and C Use spase separator: ').split()
 # d1 = (float(c) ** 2)
 # d2 = (float(a) ** 2) + (float(b) ** 2)
 # if float(d1) == float(d2):
-#     print('equilateral_triangle')
+#     print('right_triangle')
 # else:
-#     print('triangle is not equilateral')
-# empty_list = []
-print(input(list())
-print(list[::-1])
+#     print('triangle is not right')
+
+# 4
+# a = input('Please enter different values Use coma and spase separator: ').split()
+# print(list(a[::-1]))
+
+# 5
+# a = tuple(input('Please enter different values Use and spase separator: ').split())
+# b = tuple(input('Please enter again different values Use and spase separator: ').split())
+# c = a[:2] + b + a[2:]
+# print(c)
+
+# 6
+
+
+# # 7
+# import time
+#
+# a = input('Please import value of seconds: ')
+
+
+
 
 
 
